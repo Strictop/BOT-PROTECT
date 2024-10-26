4fdee9d752125c8b6aa07762c8d5972aa65f248d
Strictop <91557750+Strictop@users.noreply.github.com>
2024-10-26 20:04:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = {
         funny: ['960593732862902312', '345208352038584320'],  // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
         logChannelId: '1296627818016542753',
         logChannelIdReboot: '1299181553074704434',
+        logChannelIdBan: '1299795446562295869',
         color: '#6495ED', // Couleur en HEX | https://htmlcolorcodes.com/fr/selecteur-de-couleur/
         footer: 'Strictop-Protect', // Quand tu fais +help par exemple y'a un texte en bas de l'embed...
         maxserver: '100', // Maximum de serveur ou ton bot peut etre dedans...
