7aefb82c07b741bb5e831cef1bc976699bd59a53
Strictop <91557750+Strictop@users.noreply.github.com>
2024-11-02 15:39:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     app: {
         px: '+', // Le prefix exemple +help *help ta capter le prefix avant la commande...
-        owners: ['960593732862902312', '345208352038584320', '705403970771746828'], // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
+        owners: ['960593732862902312', '345208352038584320', '705403970771746828', '1064654693231513650'], // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
         funny: ['960593732862902312', '345208352038584320'],  // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
-        logChannelId: '1296627818016542753',
+        logChannelId: '1302279354339823657',
         logChannelIdReboot: '1299181553074704434',
         logChannelIdBan: '1300520482398797946',
         color: '#F700FF', // Couleur en HEX | https://htmlcolorcodes.com/fr/selecteur-de-couleur/
